9126c0162ce40390413327700f1c6313dbb74cfe
AlexCapilla <acapilla93@gmail.com>
2024-05-08 12:35:26 -0700

    updated game routes and established connection with game images

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Game } = require('../../models');
+const { Game, Review } = require('../../models');
 
 router.get('/', async (req, res) => {
     try {
@@ -26,4 +26,20 @@ router.get('/:id', async (req, res) => {
 
 module.exports = router;
 
-// put post delete
+// put post delete code below
+
+//POST  Ask Drew/Kyle if we need to specify '/:id' for adding new game being reviewed to storage table
+router.post('/', async (req, res) => {
+    try {
+        const {name, gameDescription, cover, rapid_id, reviewDescription, stars} = req.body;
+        const newGame = await Game.create({name, description: gameDescription, cover, rapid_id})
+        const newReview = await Review.create({description: reviewDescription, stars, user_id: req.session.user_id, game_id:newGame.id})
+        console.log(newReview)
+        if (!newGame && !newReview){
+            res.status(400).json("error in creating game review");
+        }
+        res.status(200).json("successfully created game review!");
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Game, Review, User } = require("../models");
 require("dotenv").config();
 
+// homepage
 router.get("/", async(req, res) => {
     const url = "https://opencritic-api.p.rapidapi.com/game"
   const options = {
@@ -19,3 +20,25 @@ router.get("/", async(req, res) => {
 });
 
 module.exports = router;
+
+// profile page
+router.get("/", async(req, res) => {
+  const url = "https://opencritic-api.p.rapidapi.com/game"
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": "opencritic-api.p.rapidapi.com",
+  },
+};
+
+const response = await fetch(url, options);
+const games = await response.json();
+console.log(games)
+  res.render("profile", games)
+});
+
+// login
+// signup
+// social
+// game page
